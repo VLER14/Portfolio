e9ecd26faec26fb8a8ffee1359aeb32796c8a4cb
Valeria Padron <valeria.padron02@utrgv.edu>
2024-07-20 20:18:41 -0500

    template

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Valeria</h1>
+        <p className={styles.description}>
+          I'm a recent graduate in computer science with 2 years of experience using React and
+          NodeJS.
+        </p>
+        <a href="valeriapg102@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
